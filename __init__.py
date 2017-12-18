5afc4e61dd7e0fccefbbfd2361124b6b76a33151
george <gk488@bath.ac.uk>
2017-12-18 17:02:57 +0000

    first commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+
+from ckanext.harvest.harvesters.ckanharvester import CKANHarvester
+from ckanext.harvest.harvesters.customharvester import CustomHarvester
+from ckanext.harvest.harvesters.base import HarvesterBase
+
